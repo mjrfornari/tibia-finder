88ec1d9e0b7d01e7c5b771b41ffc3c78932d0ee8
mjrfornari <mjrfornari@gmail.com>
2018-12-28 14:44:33 -0200

    asd
    asdasd

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+		<div className="App">
+			<header className="App-header">
+			<img src="https://www.tibiawiki.com.br/images/7/76/Tibia_icon.png" className="App-logo" alt="logo" />
+			<p>
+				Tibia Finder	
+			</p>
+			</header>
+		</div>
+		);
+	}
 }
 
 export default App;
